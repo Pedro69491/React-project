f62757f5a1857476c3d982b852a0a4a9ce107faa
Pedro69491 <48108176+Pedro69491@users.noreply.github.com>
2020-07-04 19:40:10 +0100

    default settings

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
